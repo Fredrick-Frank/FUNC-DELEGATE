b8ff49ca2e85a5f65fc094080309396544824c3a
Fredrick-Frank <34188600+Fredrick-Frank@users.noreply.github.com>
2022-03-02 14:36:21 +0100

    Add files via upload
    Func_Delegate is a generic delegate included in the System namespace. It has zero or more input parameters and one out parameter. The last parameter is considered as an out parameter.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Func_Delegate
+{
+    public class Program
+    {
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        static void Main()
+        {
+            Func<int, int, int> add = Sum;
+            int result = add(20, 20);
+
+            Console.WriteLine(result); // returning the third int as an output
+        }
+    }
+}
